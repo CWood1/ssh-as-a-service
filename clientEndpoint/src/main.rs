0f72734e9881f7d2f83d93d8ff81cde3c849572c
Connor Wood <connorwood71@gmail.com>
2016-04-30 15:51:35 +0100

    Ping in a loop on a thread

--- clientEndpoint/src/main.rs
+++ clientEndpoint/src/main.rs
@@ -3,13 +3,17 @@ extern crate oping;
 mod ping;
 
 fn main() {
-    let status = ping::ping_list(vec!("10.0.0.1", "4.2.2.2", "example.com"));
+    let rxstatus = ping::start_ping_list(vec!(String::from("10.0.0.1"), String::from("4.2.2.2"), String::from("example.com")));
 
-    for (host, active) in status {
-        if active {
-            println!("Host {} is active", host);
-        } else {
-            println!("Host {} is inactive", host);
+    loop {
+        let status = rxstatus.recv().unwrap();
+
+        for (host, active) in status {
+            if active {
+                println!("Host {} is active", host);
+            } else {
+                println!("Host {} is inactive", host);
+            }
         }
     }
 }
--- clientEndpoint/src/ping.rs
+++ clientEndpoint/src/ping.rs
@@ -1,12 +1,16 @@
 use oping::{Ping, PingResult, PingError};
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use std::collections::HashMap;
 use std::vec::Vec;
 
-fn ping(host: &str) -> PingResult<()> {
+fn ping(host: &String) -> PingResult<()> {
     let mut ping = Ping::new();
 
-    try!(ping.set_timeout(5.0));
-    try!(ping.add_host(host));
+    try!(ping.set_timeout(0.5));
+    try!(ping.add_host(host.as_str()));
 
     let responses = try!(ping.send());
 
@@ -19,16 +23,31 @@ fn ping(host: &str) -> PingResult<()> {
     Ok(())
 }
 
-pub fn ping_list(hosts: Vec<&str>) -> HashMap<&str, bool> {
-    let mut status: HashMap<&str, bool> = HashMap::new();
+fn ping_list(hosts: &Vec<String>) -> HashMap<String, bool> {
+    let mut status: HashMap<String, bool> = HashMap::new();
     
     for host in hosts {
-        status.insert(host, 
-                      match ping(host) {
-                          Ok(_) => true,
-                          Err(_) => false,
-                      });
+        let active = match ping(&host) {
+            Ok(_) => true,
+            Err(_) => false,
+        };
+
+        status.insert(host.clone(), active);
     }
 
     status
+}
+
+pub fn start_ping_list(hosts: Vec<String>) -> Receiver<HashMap<String, bool>> {
+    let (tx, rx): (Sender<HashMap<String, bool>>, Receiver<HashMap<String, bool>>) = mpsc::channel();
+    let thread_hosts = hosts.clone();
+
+    thread::spawn(move || {
+        loop {
+            tx.send(ping_list(&thread_hosts)).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    rx
 }
