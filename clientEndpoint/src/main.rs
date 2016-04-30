ae003f8fe1fbcba9acba55334487d24de2afd62a
Connor Wood <connorwood71@gmail.com>
2016-04-30 14:05:59 +0100

    Error handling now works correctly for ping

--- clientEndpoint/src/main.rs
+++ clientEndpoint/src/main.rs
@@ -1,23 +1,18 @@
 extern crate oping;
 
-use oping::{Ping, PingResult};
+use oping::{Ping, PingResult, PingError};
 
 fn ping(host: &str) -> PingResult<()> {
     let mut ping = Ping::new();
 
-    println!("Setting timeout");
     try!(ping.set_timeout(5.0));
-    println!("Adding host");
     try!(ping.add_host(host));
 
-    println!("Sending");
     let responses = try!(ping.send());
 
     for resp in responses {
         if resp.dropped > 0 {
-            println!("No responses from host {}", resp.hostname);
-        } else {
-            println!("Response received from {}", resp.hostname);
+            return Err(PingError::LibOpingError(String::from("No responses")));
         }
     }
 
@@ -25,5 +20,9 @@ fn ping(host: &str) -> PingResult<()> {
 }
 
 fn main() {
-    ping("4.2.2.2").expect("Error");
+    match ping("10.0.0.1") {
+        Ok(_) => println!("Ping successful!"),
+        Err(PingError::LibOpingError(e)) => println!("Error: {}", e),
+        Err(_) => println!("Unspecified error occurred"),
+    }
 }
