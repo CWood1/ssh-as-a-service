520e16f620a8fe168c4388871f8d8c153c52a7af
Connor Wood <connorwood71@gmail.com>
2016-04-30 14:23:55 +0100

    Broke ping functions into own module

--- clientEndpoint/src/main.rs
+++ clientEndpoint/src/main.rs
@@ -1,42 +1,9 @@
 extern crate oping;
 
-use oping::{Ping, PingResult, PingError};
-use std::collections::HashMap;
-use std::vec::Vec;
-
-fn ping(host: &str) -> PingResult<()> {
-    let mut ping = Ping::new();
-
-    try!(ping.set_timeout(5.0));
-    try!(ping.add_host(host));
-
-    let responses = try!(ping.send());
-
-    for resp in responses {
-        if resp.dropped > 0 {
-            return Err(PingError::LibOpingError(String::from("No responses")));
-        }
-    }
-
-    Ok(())
-}
-
-fn ping_list(hosts: Vec<&str>) -> HashMap<&str, bool> {
-    let mut status: HashMap<&str, bool> = HashMap::new();
-    
-    for host in hosts {
-        status.insert(host, 
-                      match ping(host) {
-                          Ok(_) => true,
-                          Err(_) => false,
-                      });
-    }
-
-    status
-}
+mod ping;
 
 fn main() {
-    let status = ping_list(vec!("10.0.0.1", "4.2.2.2", "example.com"));
+    let status = ping::ping_list(vec!("10.0.0.1", "4.2.2.2", "example.com"));
 
     for (host, active) in status {
         if active {
---
+++ clientEndpoint/src/ping.rs
@@ -0,0 +1,34 @@
+use oping::{Ping, PingResult, PingError};
+use std::collections::HashMap;
+use std::vec::Vec;
+
+fn ping(host: &str) -> PingResult<()> {
+    let mut ping = Ping::new();
+
+    try!(ping.set_timeout(5.0));
+    try!(ping.add_host(host));
+
+    let responses = try!(ping.send());
+
+    for resp in responses {
+        if resp.dropped > 0 {
+            return Err(PingError::LibOpingError(String::from("No responses")));
+        }
+    }
+
+    Ok(())
+}
+
+pub fn ping_list(hosts: Vec<&str>) -> HashMap<&str, bool> {
+    let mut status: HashMap<&str, bool> = HashMap::new();
+    
+    for host in hosts {
+        status.insert(host, 
+                      match ping(host) {
+                          Ok(_) => true,
+                          Err(_) => false,
+                      });
+    }
+
+    status
+}
